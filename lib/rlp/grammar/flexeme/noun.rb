9bddedb032d84590ccae308e3c0fb58cafc02232
Aleksander Pohl <apohllo@o2.pl>
2011-06-22 19:48:29 +0200

    - Restore Flexeme::Noun fields

--- lib/rlp/grammar/flexeme/noun.rb
+++ lib/rlp/grammar/flexeme/noun.rb
@@ -4,6 +4,8 @@ module Rlp
   module Grammar
     class Flexeme
       class Noun < Flexeme
+        field :lcase, :string
+        has_one :gender, :class_name => "Rlp::Grammar::Value"
       end
     end
   end
