5ce2cf7512d1657e874301196543712ed0d48934
Aleksander Pohl <apohllo@o2.pl>
2011-07-27 18:19:41 +0200

    - Add Noun requirements

--- lib/rlp/grammar/flexeme/noun.rb
+++ lib/rlp/grammar/flexeme/noun.rb
@@ -7,6 +7,10 @@ module Rlp
         field :lcase, :string
         has_one :gender, :class_name => "Rlp::Grammar::Value"
 
+        def requirements
+          @requirements ||= [{Category.for_tag(:case) => Value.for_tag(:gen)}]
+        end
+
         def values_for(form)
           self.positions_for(form).each{|vs| vs << self.paradigm.gender}
         end
